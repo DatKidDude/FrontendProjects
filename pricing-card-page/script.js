eb92b2831982cad96a26382f90c162ff23cbeab9
Craig Giddings <randomhero_345@hotmail.com>
2023-08-07 15:03:26 -0400

    Finished pricing cards

--- pricing-card-page/script.js
+++ pricing-card-page/script.js
@@ -1,1 +1,26 @@
-// write your JavaScript here
+const toggleComponent = document.querySelector(".toggle-component");
+const checkbox = toggleComponent.querySelector("input[type='checkbox']");
+const prices = document.querySelectorAll("#price");
+
+const monthly = {
+  starter: "$19",
+  pro: "$99",
+};
+
+const annually = {
+  starter: "$50",
+  pro: "$500",
+};
+
+checkbox.addEventListener("click", (e) => {
+  if (checkbox.checked) {
+    prices[0].innerText = annually.starter;
+    prices[1].innerText = annually.pro;
+  } else {
+    prices[0].innerText = monthly.starter;
+    prices[1].innerHTML = `
+    <p id="price" class="price">
+    ${monthly.pro}<span class="price-monthly">/ month</span>
+    </p>`;
+  }
+});
