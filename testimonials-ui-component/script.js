e5b5c9806cc2ebd26b1fd22981bf460e16abe575
Craig Giddings <randomhero_345@hotmail.com>
2023-08-08 00:00:26 -0400

    Finished CSS, added JSON file

--- testimonials-ui-component/script.js
+++ testimonials-ui-component/script.js
@@ -1,1 +1,68 @@
-// write your JavaScript here
+const slidesContainer = document.querySelector(".slides-container");
+const star = document.querySelector(".star");
+
+// fetch JSON data
+async function getUserData() {
+  const response = await fetch("users.json");
+  const data = await response.json();
+
+  return data;
+}
+
+try {
+  const userData = await getUserData();
+  createCardComponent(userData);
+} catch (err) {
+  console.log(err.message);
+}
+
+// create a card component
+function createCardComponent(obj) {
+  const { users } = obj;
+
+  // map JSON data to card component
+  users.map((user) => {
+    slidesContainer.innerHTML += `
+    <li class="slide">
+      <article class="slide-article">
+      <div class="user-rating">
+        <div class="star">
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div class="star">
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div class="star">
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div class="star">
+          <i class="fa-solid fa-star"></i>
+        </div>
+        <div class="star">
+          <i class="fa-solid fa-star"></i>
+        </div>
+      </div>
+      <article class="user-comment">
+        <h3>${user.title}</h3>
+        <p>
+          ${user.comment}
+        </p>
+      </article>
+      <footer class="user-profile">
+        <img
+          src="https://source.unsplash.com/${user.imageUrl}"
+          class="user-img"
+          alt="${user.username}"
+        />
+        <div class="user-info">
+          <p class="user-name">${user.username}</p>
+          <p class="user-role">${user.role}</p>
+        </div>
+      </footer>
+    </article>
+  </li>
+    `;
+  });
+}
+
+// add carousel functionality
